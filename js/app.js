bf24c045b2a28b64b2e8b9ee9e098112c7122048
justGyga <iwantdrink229@gmail.com>
2023-05-17 20:27:29 +0700

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 http.createServer(function(request,response){
      
-    response.end(table.json);
+    response.end('table.json');
      
 }).listen(3000,function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
